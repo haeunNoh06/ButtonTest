e7db91017f42a1fdcb0a91123c143422c9720a98
haeunNoh06 <okhagrace06@naver.com>
2023-04-13 20:03:38 +0900

    기본 위젯의 속성들

--- app/src/main/java/kr/hs/emirim/soeun/buttontest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/soeun/buttontest/MainActivity.kt
@@ -2,10 +2,17 @@ package kr.hs.emirim.soeun.buttontest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //var btn1 : Button
+        //btn1 = findViewById<Button>(R.id.btn1)
+//      제네릭 타입은 생략도 가능하다
+
+
+        setContentView(R.layout.layout3)
     }
 }
